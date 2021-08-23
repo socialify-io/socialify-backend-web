f8529f08b759002f8fb7e14e35fd4fddd43aaca3
arin <67313390+6Arin9@users.noreply.github.com>
2021-08-23 22:49:52 +0200

    Error Reports Endpoint

--- db/db.js
+++ db/db.js
@@ -1,10 +1,13 @@
 var sqlite3 = require('sqlite3').verbose();
 var users = new sqlite3.Database("./db/users.db");
+var error_reports = new sqlite3.Database("./db/error_reports.db");
 
 var create_users_db = require('./create_users_db');
+var create_error_reports_db = require('./create_error_reports_db');
 
 function db_setup() {
     create_users_db(users);
+    create_error_reports_db(error_reports);
 }
 
-module.exports = { db_setup, users };
+module.exports = { db_setup, users, error_reports };
