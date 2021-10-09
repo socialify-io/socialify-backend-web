9d4c653e43aa55c2f419d36b4660fffdd317410f
Pengwius <tomekftomekf@gmail.com>
2021-10-09 20:23:12 +0200

    More organized routing

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from 'express';
-import { Request, Response } from 'express';
-import getKeyRouter = require('./router/getKey')
+import { Request, Response, Router } from 'express';
+import router from './router/router';
 
-const app = express();
+const app: express.Application = express();
 
 app.set('port', 3000)
 
 app.use(express.static('static'))
 
-app.use('/api', getKeyRouter);
+app.use('/api', router);
 app.use((req: Request, res: Response) => {
   res.status(404).sendFile("templates/what_are_you_looking_for.html", {root: `${__dirname}//..//static//`});
 });
