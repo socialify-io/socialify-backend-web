f092cc4e1ff8c7c8100ed1bb04cfccbbb5184bbd
Pengwius <tomekftomekf@gmail.com>
2021-10-09 15:44:02 +0200

    404 default error

--- app.ts
+++ app.ts
@@ -1,8 +1,14 @@
 import express from 'express';
-// rest of the code remains same
+
 const app = express();
-const PORT = 8000;
-app.get('/', (req, res) => res.send('Express + TypeScript Server'));
+const PORT = 3000;
+
+app.use(express.static('static'))
+
+app.use((req, res) => {
+  res.status(404).sendFile("templates/what_are_you_looking_for.html", {root: `${__dirname}//static//`});
+});
+
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
 });
